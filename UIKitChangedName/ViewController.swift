8118919d14e32aa8c24d0160c21cf4fc9b768113
Daniel Sosa <danisosachile@gmail.com>
2024-07-07 12:55:12 -0400

    Initial commit

---
+++ UIKitChangedName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitProjectBase
+//
+//  Created by Daniel Sosa on 06-07-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
